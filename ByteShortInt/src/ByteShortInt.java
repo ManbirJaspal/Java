ebfce06a33ba202e551428111824694d9ef00fd9
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-04-29 17:32:14 -0400

    variable types

---
+++ ByteShortInt/src/ByteShortInt.java
@@ -0,0 +1,16 @@
+public class ByteShortInt {
+    public static void main(String[] args){
+        // int has a width of 32
+        int myMinValue = -2_147_483_648; //min and max values that int can contain
+        int myMaxValue = 2_147_483_647;
+
+        //byte has a width of 8
+        byte myByteValue = -128;
+
+        //short has a width of 16
+        short myShortValue = 32767; //max value
+
+        //long has a width of 64
+        long myLongValue = 9_223_372_036_854_775_807L; //max value
+    }
+}
